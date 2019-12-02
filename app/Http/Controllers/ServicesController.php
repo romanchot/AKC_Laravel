b192bb8ff82b09023256d0c44951f89637c54528
Roman Gutierrez <roman710@gmail.com>
2019-12-02 12:14:46 -0500

    Add Security ServiceController and README file Changed --SURE2

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -7,7 +7,22 @@ use App\Service;
 
 class ServicesController extends Controller
 {
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+      $this->middleware('auth');
+  }
 
+  /**
+   * Show the application dashboard.
+   *
+   * @return \Illuminate\Contracts\Support\Renderable
+   */
+   
   public function index()
   {
       $services = Service::all();
